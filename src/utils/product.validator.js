b18c4fd5ae692c80bbb4e5cf04eae4f53d5f43a4
zoitovfirdavs3-eng <zoitovfirdavs3@gmail.com>
2026-01-14 13:13:33 +0500

    Add productValidor codes

--- src/utils/product.validator.js
+++ src/utils/product.validator.js
@@ -5,7 +5,10 @@ module.exports = (req, res, next) => {
         let product = req.body;
         if(!product.name) throw new ClientError(`Product name is required`, 400);
         if(!product.price) throw new ClientError(`Product price is required`, 400);
+        if(product.price.isNaN()) throw new ClientError(`Product price is NaN`, 400);
+        if(product.price == 0) throw new ClientError(`Product price is not valid`, 400);
         if(!product.count) throw new ClientError(`Product count is required`, 400);
+        if(product.count == 0) throw new ClientError(`Product count is not valid`, 400);
         return next();
     } catch (err) {
         return globalError(err, res);
